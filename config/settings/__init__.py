1afaa427dc440df4f913edb72176e7c8a4cd4709
alextamboli <alex.tamboli8@gmail.com>
2023-12-05 22:36:34 +0530

    Adding render deployement config

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 
 if os.environ.get("ENVIRONMENT") == 'Production':
-    from .development import *
-    # from .production import *
+    # from .development import *
+    from .production import *
 else:
     from .development import *
